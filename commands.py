11b68defcf0fc1139c25759ade830ba1612411b3
brianlball <Brian.Ball@nrel.gov>
2021-06-24 11:28:09 -0600

    add commands for ubuntu box

---
+++ commands.py
@@ -0,0 +1,20 @@
+import requests
+
+http://10.40.7.78:5002/objects/ee349ca0-f8aa-4b83-9fc6-86d727399914
+     
+r = requests.get('http://10.40.7.78:5002/', params = {"_id": "ee349ca0-f8aa-4b83-9fc6-86d727399914"})
+r.text
+
+r = requests.put('http://10.40.7.78:5002/update', json = {"_id": "ee349ca0-f8aa-4b83-9fc6-86d727399914", "fan": "n"})
+r.text
+
+HP_json = '{ "_id": "ee349ca0-f8aa-4b83-9fc6-86d727399914", "dis": "Unitary HP", "fan": "m", "heatPump": "m", "siteRef": "6b25c3c7-39e4-4be7-84a9-17e80feecaf5"}'
+r = requests.put('http://10.40.7.78:5002/', data = HP_json)
+r.text
+ 
+HP_json = '{ "_id": "ee349ca0-f8aa-4b83-9fc6-86d727399914", "dis": "Unitary HP", "equip": "m", "heatPump": "m", "siteRef": "6b25c3c7-39e4-4be7-84a9-17e80feecaf5"}'
+r = requests.post('http://10.40.7.78:5002/', data = HP_json)
+r.text
+ 
+r = requests.delete('http://10.40.7.78:5002/', params = {"_id": "ee349ca0-f8aa-4b83-9fc6-86d727399914"})
+r.text 
