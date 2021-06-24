21b6f63fd5bc207cb6bac792e69b86df9234caa8
ubuntu <brian.ball@nrel.gov>
2021-06-24 11:29:05 -0600

    get to work on ubuntu box

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ from flask_mongoengine import MongoEngine
 app = Flask(__name__)
 
 app.config['MONGODB_SETTINGS'] = {
-    'db': 'your_database',
-    'host': 'localhost',
-    'port': 27017
+        'connect': False,
+        'host': 'mongodb://127.0.0.1:27018/test_db?authSource=admin'
 }
+
 db = MongoEngine()
 db.init_app(app)
 
